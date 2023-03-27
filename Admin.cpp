73efe0c91fcc3fbfd44e9e184e03ead1c6ec3e94
Shivsharan Malage <104545490+Shivmalge@users.noreply.github.com>
2023-03-27 17:15:15 +0530

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -4,6 +4,80 @@
 #include <string.h>
 using namespace std;
 
+
+Admin::Admin() {
+	
+}
+
+
+
+void Admin::createProcess() 
+{
+	Process* newProcess = new Process();
+	string name;
+	int burst;
+	cout << "Enter the name of the process: ";
+	cin >> name;
+	newProcess.setname(name);
+
+	cout << "Enter the time cycles required to complete the process: ";
+	cin >> burst;
+	newProcess.setBurstTime(burst);
+
+	//cout << "Enter the time cycles for the process to go to wait state: ";
+	//cin >> newProcess.timeToWait;
+	processList.push_back(newProcess);
+	cout << "Process added successfully." << endl;
+}
+
+
+void Admin::displayProcess() 
+{
+	cout << "Process List:" << endl;
+	for (auto& process : processList) {
+		cout << "Name: " << process.name<< " Burst Time  " << process.burst << endl;
+	}
+}
+
+
+void Admin::updateProcess(int id)
+{
+
+	for (auto& process : processList)
+		{
+			if (process.getiD() == id)
+			{
+				cout << "Enter the name of the process: ";
+				cin >> process.name;//if not worked the use getter setter
+				cout << "Enter the burst time for the process: ";
+				cin >> process.burst;
+				//cout << "Enter the time cycles for the process to go to wait state: ";
+				//cin >> process.time;
+				cout << "Process updated successfully." << endl;
+			}
+		}	cout << "update failed please check the process ID." << endl;
+}
+
+void Admin::deleteProcess(int id, int index)
+{
+	for (auto& process : processList)
+		{
+			if (process.getiD() == id)
+			{
+				processList.erase(processList.begin() + index);
+				cout << "Process deleted successfully." << endl;
+			}
+		}
+}
+
+void Admin::view_processes()
+{	
+	for (int i = 0; i < processList.size(); i++)
+	{
+		cout << "The process ID is " << processList[i].getiD() << ", process name is " << processList[i].getname() << " and the process burst time is " << processList[i].getBurstTime() << endl;
+	}
+}
+
 //Function to display Admin menu
 void Admin::processAdminMenu(void)
 {
