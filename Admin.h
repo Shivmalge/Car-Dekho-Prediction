6cd84e0b718d2d8e32de81d61b6a5f915d51db09
Shivsharan Malage <104545490+Shivmalge@users.noreply.github.com>
2023-03-31 16:06:19 +0530

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,23 +1,42 @@
 #pragma once
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <queue>
+
+#include "Process.h"
+
 using namespace std;
 
-class Admin
-{
+class Admin: public Process
+{	
 
 private:
-	vector<Process> processList;
-public:
-	
-	
+	static Admin* admin;
 	Admin();
+	~Admin();	
+
+public:
+	queue<Process> process_queue;
+	static Admin* getInstan()
+	{
+		if (admin == 0)
+		{
+			admin = new Admin();
+		}
+		return admin;
+	}
+
+	vector<Process> processList;
 	void ProcessAdminMenu(void);
 	void view_processes();
 	void createProcess();
 	void displayProcess();
-	void updateProcess(int id);
-	void deleteProcess(int id, int index);
-	~Admin();
-};
+	void updateProcess();
+	void deleteProcess(int index);
+	void selectProcess(int count);
+	int get_random_status()
+	
 
+		
+};
