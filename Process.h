15fd6c48c9e1b2701b54f3c8dfc495f54cbe362c
Shivsharan Malage <104545490+Shivmalge@users.noreply.github.com>
2023-03-31 17:02:56 +0530

    Update Process.h

--- Process.h
+++ Process.h
@@ -1,19 +1,37 @@
+
 #pragma once
-#include<iostream>
-#include<string.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+
 using namespace std;
 
 class Process
-{
+{	
 public:
+
+	enum ProcessState { READY, WAITING, STOPPED, COMPLETED };
+
 	int ID;                  //process ID
 	int num;
+	int quantum;
 	int rem_time;           //Remaining time
-	string status;          //Status of process
-	int no_of_cycles_req; 
 	string name;
 	int burst;
-	int timeToWait;
+	int state; // 1 = Ready to Run, 2 = Waiting, 3 = Stopped, 4 = Completed
+	int timeRemaining;
+	int timeInReady;
+	int timeInWaiting;
+	int timeInStopped;
+	int timeInCompleted;
+	int cpuId;
+
+	int time_for_completion;
+	int time_for_waiting;
+
+
+	int time_spent[4] = { 0, 0, 0, 0 }; //{ready_to_run, waiting, stopped, completed}
+	int time_cycle[2];
 
 	Process();
 
@@ -23,15 +41,12 @@ public:
 	void setBurstTime(int burst);
 	int getBurstTime(void);
 
-
-
 	
 	//void setID(int ID);
-	int getiD();;
+	int getiD();
 	//void setnoofprocess(int num);
 	//int getnoofprocess(void);
-	Process() {
-
-	}
+	
+	void run_process(vector <Process> processList);
+	int get_random_status();
 };
-
