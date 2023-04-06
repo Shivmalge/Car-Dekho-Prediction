1b9e8ffc3f5516417e990ccc4baaf31d841ee28a
Shivsharan Malage <104545490+Shivmalge@users.noreply.github.com>
2023-04-06 15:10:08 +0530

    Update User.cpp

--- PROCESS/User.cpp
+++ PROCESS/User.cpp
@@ -27,32 +27,41 @@ void User::ProcessUserMenu(Admin* admin)
 {
 	cout<<"\n--------------------------- User MENU ---------------------------------\n";
 	cout << "Select what you want to perform: \n";
-	cout << "01. view all process\n 02. Select process\n 0. Quit\n ";
+	cout << "01. view all process\n 02. Select process\n 3. Quit\n ";
 
 	int choice;
-	cout << "\nEnter your choice: ";
-	cin >> choice;
-	if (choice < 0 || choice >2)
+	while (true)
 	{
-		cout << "\nError: Invalid option chosen " << choice << " Try Again..\n"<<endl;
-	}
+		cout << "\nEnter your choice: ";
+		cin >> choice;
+		if (choice < 0 || choice >3)
+		{
+			cout << "\nError: Invalid option chosen " << choice << " Try Again..\n" << endl;
+		}
 
-	else if (choice == 1)
-	{
-		admin->view_processes();
-	}
+		else if (choice == 1)
+		{
+			admin->view_processes();
+		}
 
+
+		else if (choice == 2)
+		{
+			int count;
+			cout << "\nEnter no of processes you want to run" << endl;
+			cin >> count;
+			admin->selectProcess(count);
+		}
+
+		else if(choice == 3)
+		{
+			break;
+		}
+
+		else
+		{
+			cout << "Functionalities yet to be updated: " << endl;
+		}
+	}
 	
-	else if (choice == 2)
-	{	
-		int count;
-		cout << "Enter no of processes" << endl;
-		cin >> count;
-		admin->selectProcess(count);
-	}
-
-	else
-	{
-		cout << "\nFunctionalities yet to be updated..";
-	}
 }
